656c0a4a815aa2cdd24fc440d6a2095fccc7836e
codelive <jlive@ic-itcr.ac.cr>
2019-08-28 15:43:34 -0600

    update1

--- tarea1_analisis_de_algoritmos/Programa.java
+++ tarea1_analisis_de_algoritmos/Programa.java
@@ -12,33 +12,55 @@ import javax.swing.JOptionPane;
 
 public class Programa
 {
-    
-    ArrayList<Cajita> matriz = new ArrayList<Cajita>();
-    int totales_finales_lamina = 0;
-    List<integer> lista_final_lamina = new ArrayList<Integer>();
-    
-    public void main(String[] args) {
+    //Variable globales
+    ArrayList<Cajita> matriz = new ArrayList<>();
+    ArrayList<Cajita> lista_final_lamina = new ArrayList<>();
+    int totales_finales_lamina = lista_final_lamina.size();
+
+    /*
+        Empieza programa con la clase main
+    */
+    public void main(String[] args) 
+    {
+        //Pido datos
         String cadena=JOptionPane.showInputDialog("Digite su diametro del tablero");
         JOptionPane.showMessageDialog(null, cadena);
         int diametro = convertIntoInt(cadena);
         
-        if(diametro%2 != 1){
-            
+        //Evaluo que solo lados pares entran
+        while(diametro%2 != 0)
+        {
+            //hago la salida del programa y vuelvo a preguntar de nuevo
+            cadena=JOptionPane.showInputDialog("Digite su diametro del tablero");
+            JOptionPane.showMessageDialog(null, cadena);       
         }
         
+        //Inicio programa
         crearTablero(diametro);
+        
+        //Continuo prorama
+        crearTipoFigura();
+        
+        //
         
     }
     
+    /*
+        Al crear el tipo de figura, me base en la clase TipoFigura
+    */
     private void crearTipoFigura()
     {
         //se hace modulo 10
-        totales_finales_centrales %= 10;
+        totales_finales_lamina %= 10;
         
         //inserto
         
     }
     
+    /*
+        Se inicializa el tablero donde se coloca las posiciones 
+        de las figuras. 
+    */
     private void crearTablero(int diametro)
     {
         for(int i=0; matriz.size() > i; i++)
@@ -52,6 +74,9 @@ public class Programa
         Lamina p_lamina = new Lamina(matriz);
     }
      
+    /*
+        Se crea funcion para transformas String a Integer
+    */
     private int convertIntoInt(String param)
     {
         return Integer.parseInt(param);
--- tarea1_analisis_de_algoritmos/TipoFigura.java
+++ tarea1_analisis_de_algoritmos/TipoFigura.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tarea1_analisis_de_algoritmos;
 
-/**
- *
- * @author Jerem
- */
 public class TipoFigura 
 {
     private int id, largo, ancho;
